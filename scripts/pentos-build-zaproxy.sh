baa0f3c04f5d9a366ef0d2b33649b9bac243b00c
cyberxml <cyberxml@mail.com>
2015-12-03 22:06:57 -0700

    build zaproxy

---
+++ scripts/pentos-build-zaproxy.sh
@@ -0,0 +1,7 @@
+# OWASP Zed Attack Proxy 
+
+cd /opt
+wget https://github.com/zaproxy/zaproxy/releases/download/2.4.2/ZAP_2.4.2_Linux.tar.gz
+
+tar xzf ZAP_2.4.2_Linux.tar.gz
+rm ZAP_2.4.2_Linux.tar.gz
