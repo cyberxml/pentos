b92f78b86887d7baae321dc28b6f960fbf18128b
cyberxml <cyberxml@mail.com>
2015-12-03 21:36:58 -0700

    build squirrelsql

---
+++ scripts/pentos-build-squirrelsql.sh
@@ -0,0 +1,12 @@
+# Squirrel SQL - database browswer
+
+cd /opt
+mkdir /opt/squirrelsql
+cd /opt/squirrelsql
+
+wget http://downloads.sourceforge.net/project/squirrel-sql/1-stable/3.7.0/squirrel-sql-3.7-standard.jar
+
+echo java -jar /opt/squirrelsql/squirrel-sql-3.7-standard.jar > squirrelsql.sh
+
+chmod 755 squirrelsql.sh
+
