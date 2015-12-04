37858bb6ef6196d77826f358d15466f693077b44
cyberxml <cyberxml@mail.com>
2015-12-03 22:47:11 -0700

    build openscap

--- scripts/pentos-build-openscap.sh
+++ scripts/pentos-build-openscap.sh
@@ -4,8 +4,11 @@
 # /usr/local/bin/oscap
 yum -y install libcurl-devel
 yum -y install libxml2-devel
-yum -y install libxslt
-yum -y install pcre
+yum -y install libxslt-devel
+yum -y install pcre-devel
+yum -y install bzip2-devel
+yum -y install libtool
+yum -y install swig
 
 cd /opt
 git clone https://github.com/OpenSCAP/openscap
