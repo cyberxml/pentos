a8f833387992cd1bfdf1136566d1b4523891e573
cyberxml <cyberxml@mail.com>
2015-12-03 21:17:42 -0700

    update john

--- scripts/pentos-build-john.sh
+++ scripts/pentos-build-john.sh
@@ -2,9 +2,12 @@
 # John the Ripper
 #----------------------------------
 # /opt/john-1.8.0-jumbo-1/run/john
+yum -y install openssl-devel
+
 cd /opt
 wget http://www.openwall.com/john/j/john-1.8.0-jumbo-1.tar.gz
 tar xvzf john-1.8.0-jumbo-1.tar.gz
-cd john-1.8.0-jumbo-1
+cd /opt/john-1.8.0-jumbo-1/src
 ./configure
 make
+make install
