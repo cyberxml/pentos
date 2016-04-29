7fa8471badd88215739d6edbe71d3a91f8e53364
cyberxml <cyberxml@mail.com>
2016-04-29 13:03:59 -0600

    local install and path

--- build/pentos-build-john.sh
+++ build/pentos-build-john.sh
@@ -9,6 +9,11 @@ wget http://www.openwall.com/john/j/john-1.8.0-jumbo-1.tar.gz
 tar xvzf john-1.8.0-jumbo-1.tar.gz
 rm john-1.8.0-jumbo-1.tar.gz
 cd /opt/john-1.8.0-jumbo-1/src
-./configure
+./configure --disable-openmp
 make
 make install
+mkdir /usr/local/john
+mkdir /usr/local/john/bin
+cp -rp /opt/john-1.8.0-jumbo-1/run/* /usr/local/john/bin 
+
+echo "pathmunge /usr/local/john/bin" > /etc/profile.d/pentos-john.sh
