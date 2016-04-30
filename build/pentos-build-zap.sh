68f4823ee656d0ade9593fe5f3c17925eab90597
cyberxml <cyberxml@mail.com>
2016-04-30 07:07:39 -0600

    bump zap version to 2.4.3

--- build/pentos-build-zap.sh
+++ build/pentos-build-zap.sh
@@ -1,9 +1,9 @@
 # OWASP Zed Attack Proxy 
 
 cd /opt
-wget https://github.com/zaproxy/zaproxy/releases/download/2.4.2/ZAP_2.4.2_Linux.tar.gz
+wget https://github.com/zaproxy/zaproxy/releases/download/2.4.3/ZAP_2.4.3_Linux.tar.gz
 
-tar xzf ZAP_2.4.2_Linux.tar.gz
-rm ZAP_2.4.2_Linux.tar.gz
+tar xzf ZAP_2.4.3_Linux.tar.gz
+rm ZAP_2.4.3_Linux.tar.gz
 
-echo "pathmunge /opt/ZAP_2.4.2" > /etc/profile.d/pentos-zap.sh
+echo "pathmunge /opt/ZAP_2.4.3" > /etc/profile.d/pentos-zap.sh
