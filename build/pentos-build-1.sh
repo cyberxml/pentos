fe2e54951c509ba4f958cae6347b42aa3d31ae9c
cyberxml <cyberxml@mail.com>
2016-04-30 07:05:19 -0600

    incorporate selinux disable into build-1

--- build/pentos-build-1.sh
+++ build/pentos-build-1.sh
@@ -6,4 +6,6 @@ ifup eth0
 # add penne to the sudoers
 echo "penne    ALL=(ALL)       ALL" >> /etc/sudoers
 
-
+# disable SELinux for OPENVAS
+sed -i -e 's@^SELINUX=.*$@SELINUX=disabled@' /etc/selinux/config
+echo you must reboot the system to complete the selinux configuration
