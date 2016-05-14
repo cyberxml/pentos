f657e64f61a2620cbca5faea90ec70897d892ad2
root <root@pos.(none)>
2016-05-13 18:59:19 -0600

    chmod corrections

--- build/pentos-build-john.sh
+++ build/pentos-build-john.sh
@@ -14,7 +14,7 @@ make
 make install
 
 chgrp -hR pentester /opt/john-1.8.0-jumbo-1
-chmod 750 john-1.8.0-jumbo-1/
-chmod 770 john-1.8.0-jumbo-1/run
+chmod 750 /opt/john-1.8.0-jumbo-1/
+chmod 770 /opt/john-1.8.0-jumbo-1/run
 
 echo "pathmunge /opt/john-1.8.0-jumbo-1/run" > /etc/profile.d/pentos-john.sh
