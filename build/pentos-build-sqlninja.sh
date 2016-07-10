0ef5f8122e3b620426647cca797089cac6d3cf6f
cyberxml <cyberxml@mail.com>
2016-07-09 20:23:16 -0600

    new tools

---
+++ build/pentos-build-sqlninja.sh
@@ -0,0 +1,3 @@
+cd /opt
+wget http://downloads.sourceforge.net/project/sqlninja/sqlninja/sqlninja-0.2.6-r1.tgz
+tar xzf sqlninja-0.2.6-r1.tgz
---
+++ build/pentos-build-w3af.sh
@@ -0,0 +1,2 @@
+cd /opt
+git clone --depth 1 https://github.com/andresriancho/w3af.git
