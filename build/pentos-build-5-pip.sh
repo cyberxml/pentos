ec5a7ab62e175a1c6bf9250794dd28f94c0ab9c4
root <root@localhost.localdomain>
2017-07-13 13:40:10 -0400

    trusted host for SSL proxy

--- build/pentos-build-5-pip.sh
+++ build/pentos-build-5-pip.sh
@@ -1,19 +1,21 @@
 
-pip install --upgrade pip
-pip3 install --upgrade pip
+pip install --upgrade --trusted-host pypi.python.org pip
+pip3 install --upgrade --trusted-host pypi.python.org pip
 
 # something wrong with pip compile environment
 cp /usr/lib/python2.7/site-packages/packaging/requirements.py /usr/lib/python2.7/site-packages/packaging/requirements.py-orig
 cp /opt/pentos/build/requirements.py /usr/lib/python2.7/site-packages/packaging/requirements.py
 
-pip install binwalk
-pip install awsscout2
-pip install crackmapexec
-pip install autopwn
-pip install trueseeing
-pip install harvester
-pip install scrapy
-pip install sslyze
+pip install --trusted-host pypi.python.org binwalk
+pip install --trusted-host pypi.python.org incremental
+pip install --trusted-host pypi.python.org awsscout2
+pip install --trusted-host pypi.python.org crackmapexec
+pip install --trusted-host pypi.python.org autopwn
+pip install --trusted-host pypi.python.org trueseeing
+pip install --trusted-host pypi.python.org harvester
+pip install --trusted-host pypi.python.org scrapy
+pip install --trusted-host pypi.python.org sslyze
+pip install --trusted-host pypi.python.org olefile
 
 # directory structure with multiple hooks
 # need to test; nmap is failing
